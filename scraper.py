a576ded92d95c84f7c2324fababaada980441acc
tiagodeazevedoferreira <tiago.aferreira@gmail.com>
2025-06-03 08:47:40 -0300

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from selenium import webbdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
